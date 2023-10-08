be3660dd05d73c246fb86f0cd18825cdd7cfd6ee
lucarauj <lukas.martins193@gmail.com>
2023-10-08 15:42:59 -0300

    Configuração do Processor

--- sistema-de-pagamento/src/main/java/Transacao.java
+++ sistema-de-pagamento/src/main/java/Transacao.java
@@ -1,6 +1,8 @@
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public record Transacao (
         Long id,
@@ -14,4 +16,51 @@ public record Transacao (
         String nomeDaLoja
 
 ) {
+    public Transacao withValor(BigDecimal valor) {
+        return new Transacao(
+                this.id(),
+                this.tipo(),
+                this.data(),
+                valor,
+                this.cpf(),
+                this.cartao(),
+                this.hora(),
+                this.donoDaLoja(),
+                this.nomeDaLoja()
+        );
+    }
+
+    public Transacao withData(String data) throws ParseException {
+        var dateFormat = new SimpleDateFormat("yyyyMMdd");
+        var date = dateFormat.parse(data);
+
+        return new Transacao(
+                this.id(),
+                this.tipo(),
+                new Date(date.getTime()),
+                this.valor(),
+                this.cpf(),
+                this.cartao(),
+                this.hora(),
+                this.donoDaLoja(),
+                this.nomeDaLoja()
+        );
+    }
+
+    public Transacao withHora(String hora) throws ParseException {
+        var dateFormat = new SimpleDateFormat("HHmmss");
+        var date = dateFormat.parse(hora);
+
+        return new Transacao(
+                this.id(),
+                this.tipo(),
+                this.data(),
+                this.valor(),
+                this.cpf(),
+                this.cartao(),
+                new Time(date.getTime()),
+                this.donoDaLoja(),
+                this.nomeDaLoja()
+        );
+    }
 }
