3785916cacf2538aa62a008aa326d789a94f7037
lucarauj <lukas.martins193@gmail.com>
2023-10-08 15:21:58 -0300

    Configuração do Reader

--- sistema-de-pagamento/src/main/java/BatchConfig.java
+++ sistema-de-pagamento/src/main/java/BatchConfig.java
@@ -7,8 +7,12 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -37,6 +41,35 @@ public class BatchConfig {
                 .processor(processor)
                 .writer(writer)
                 .build();
+    }
 
+    @Bean
+    FlatFileItemReader<TransacaoCNAB> reader() {
+        return new FlatFileItemReaderBuilder<TransacaoCNAB>()
+                .name("reader")
+                .resource(new FileSystemResource("files\\CNAB.txt"))
+                .fixedLength()
+                .columns(
+                        new Range(1, 1),
+                        new Range(2, 9),
+                        new Range(10, 19),
+                        new Range(20, 30),
+                        new Range(31, 42),
+                        new Range(43, 48),
+                        new Range(49,62),
+                        new Range(63, 80)
+                )
+                .names(
+                        "tipo",
+                        "data",
+                        "valor",
+                        "cpf",
+                        "cartao",
+                        "hora",
+                        "donoDaLoja",
+                        "nomeDaLoja"
+                )
+                .targetType(TransacaoCNAB.class)
+                .build();
     }
 }
--- sistema-de-pagamento/src/main/java/Transacao.java
+++ sistema-de-pagamento/src/main/java/Transacao.java
@@ -3,14 +3,15 @@ import java.sql.Date;
 import java.sql.Time;
 
 public record Transacao (
-                Long id,
-                Integer tipo,
-                Date data,
-                BigDecimal valor,
-                Long cpf,
-                String cartao,
-                Time hora,
-                String donoDaLoja,
-                String nomeDaLoja
-        )
-{ }
+        Long id,
+        Integer tipo,
+        Date data,
+        BigDecimal valor,
+        Long cpf,
+        String cartao,
+        Time hora,
+        String donoDaLoja,
+        String nomeDaLoja
+
+) {
+}
